ecbf9cdda9eddfe13269966efd3ab55a23c2eacb
rafael <rafaelwildfire@gmail.com>
2024-08-05 21:08:24 -0300

    fix: ajuste pdf

--- src/components/Home/SaleSection.vue
+++ src/components/Home/SaleSection.vue
@@ -14,7 +14,7 @@
                 variant="outlined"
                 class="mb-1"
                 target="_blank"
-                href="src/assets/pdfs/caopastor20.pdf"
+                href="src/assets/caopastor20.pdf"
               ></v-btn>
             </div>
             <!-- <div class="package-discount text-center">
